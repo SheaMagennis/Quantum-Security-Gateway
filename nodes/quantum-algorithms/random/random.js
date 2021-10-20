0d9b3525bd0f32fd94f4a0f6acbf2f8842cbdf94
SheaMagennis <55768880+SheaMagennis@users.noreply.github.com>
2021-10-20 13:06:56 +0100

    new folder for true-random algotithm

---
+++ nodes/quantum-algorithms/random/random.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const util = require('util');
+const snippets = require('../../snippets');
+const errors = require('../../errors');
+const logger = require('../../logger');
+const {PythonInteractive, PythonPath} = require('../../python');
+const shell = new PythonInteractive(PythonPath);
+
+module.exports = function(RED) {
+  function RandomNode(config) {
+    RED.nodes.createNode(this, config);
+    this.name = config.name || 'Random';
+    const node = this;
+
+    logger.trace(this.id, 'Initialised random');
+
+    this.on('input', async function(msg, send, done) {
+      logger.trace(node.id, 'random received input');
+
+      let error = errors.validateGroversInput(msg);
+      if (error) {
+        logger.error(node.id, error);
+        done(error);
+        return;
+      }
+
+      let script = util.format(snippets.GROVERS, msg.payload);
+
+      shell.start();
+      await shell.execute(script)
+          .then((data) => {
+            data = data.split('\n');
+            msg.payload = {
+              topMeasurement: data[0].trim(),
+              iterationsNum: Number(data[1]),
+            };
+            send(msg);
+            done();
+          }).catch((err) => {
+            logger.error(node.id, err);
+            done(err);
+          }).finally(() => {
+            logger.trace(node.id, 'Executed grovers command');
+            shell.stop();
+          });
+    });
+  }
+  RED.nodes.registerType('random', RandomNode);
+};
