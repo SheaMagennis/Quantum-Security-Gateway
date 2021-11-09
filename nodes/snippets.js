50be8cbc990944f9a91173633209b8d66cd3399f
SheaMagennis <55768880+SheaMagennis@users.noreply.github.com>
2021-11-09 15:26:01 +0000

    Added intial QSVM implementation into snippets

--- nodes/snippets.js
+++ nodes/snippets.js
@@ -304,6 +304,51 @@ dec = list(counts.keys())[0]
 print(int(dec,2))
 `;
 
+const QSVM =
+`import numpy as np
+import csv
+from qiskit import Aer
+from qiskit.aqua import QuantumInstance, aqua_globals
+from qiskit.circuit.library import ZZFeatureMap 
+from qiskit.aqua.components.multiclass_extensions import (ErrorCorrectingCode,AllPairs,OneAgainstRest)
+from qiskit.aqua.algorithms import QSVM
+from qiskit.aqua.utils import get_feature_dimension
+from qiskit import IBMQ
+
+vals=[]
+with open("UNSW_NB15_training_ten.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for lines in csv_reader:
+      vals.append(list(lines))
+
+test = []
+for i in vals:
+    subvals=[]
+    subvals.append(float(i[7])/1000)
+    subvals.append(float(i[16])*100)
+    test.append(subvals)
+    
+training_data = {'A': np.asarray([test[0],test[1],test[2],test[3],test[4]]),'B': np.asarray([test[5],test[6],test[7],test[8]])}
+
+backend = Aer.get_backend('qasm_simulator')
+
+num_qubits = 2
+shots = 8192 
+
+feature_map = ZZFeatureMap(feature_dimension=num_qubits,reps=2,entanglement='full')
+
+svm = QSVM(feature_map, training_data,testing_data) 
+quantum_instance = QuantumInstance(backend,shots=shots,skip_qobj_validation=False)
+
+result = svm.run(quantum_instance) 
+
+data = np.array([[1.453,0.5],[1.023,0.5],[0.135,0.5],[0.266,0.5]]) #Unlabelled test data
+
+prediction = svm.predict(data,quantum_instance) # Predict using unlabelled data 
+print(prediction)
+`;
+
 module.exports = {
   IMPORTS,
   QUANTUM_CIRCUIT,
@@ -341,4 +386,5 @@ module.exports = {
   SHORS,
   HISTOGRAM,
   RAND,
+  QSVM,
 };
